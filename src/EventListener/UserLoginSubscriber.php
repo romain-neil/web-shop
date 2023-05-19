ace626a362510c0f019938727d388d9fa4b0b66e
Romain Neil <contact@romain-neil.fr>
2023-05-19 17:37:56 +0200

    feat: user login subscriber

---
+++ src/EventListener/UserLoginSubscriber.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
+
+class UserLoginSubscriber implements EventSubscriberInterface {
+
+	private EntityManagerInterface $em;
+
+	public function __construct(EntityManagerInterface $em) {
+		$this->em = $em;
+	}
+
+	public static function getSubscribedEvents(): array {
+		return [
+			LoginSuccessEvent::class => 'onLoginSuccess'
+		];
+	}
+
+	public function onLoginSuccess(LoginSuccessEvent $event): void {
+		/** @var User $user */
+		$user = $event->getUser();
+
+		$session = $event->getRequest()->getSession();
+		$session->set('lastLoginDate', $user->getLastLoginDate());
+
+		$user->setLastLoginDate(new \DateTime('now'));
+
+		$this->em->persist($user);
+		$this->em->flush();
+	}
+
+}
