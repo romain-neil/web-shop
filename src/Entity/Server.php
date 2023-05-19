58b8845c69355ec5e8af5b37e37d9d5e8f361ec7
Romain Neil <contact@romain-neil.fr>
2023-05-19 17:37:08 +0200

    feat: implement used class

--- src/Entity/Server.php
+++ src/Entity/Server.php
@@ -8,7 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ServerRepository::class)]
-class Server {
+class Server implements \Stringable {
 
 	#[ORM\Id]
 	#[ORM\GeneratedValue]
