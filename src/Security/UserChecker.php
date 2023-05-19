c682bf03cdaaa387d5553a20a6c80237c2e5d626
Romain Neil <contact@romain-neil.fr>
2023-05-19 17:38:03 +0200

    feat: user checker

---
+++ src/Security/UserChecker.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Security;
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
+use Symfony\Component\Security\Core\Exception\DisabledException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserChecker implements UserCheckerInterface {
+
+	public function checkPreAuth(UserInterface $user): void {
+		if(!$user instanceof User) {
+			return;
+		}
+
+		if(!$user->getIsActivated()) {
+			throw new CustomUserMessageAccountStatusException("Le compte n'est pas activé. Merci de cliquer sur le lien de première connexion");
+		}
+	}
+
+	public function checkPostAuth(UserInterface $user): void {
+		if(!$user instanceof User) {
+			return;
+		}
+
+		if(!$user->getIsActivated()) {
+			throw new DisabledException();
+		}
+	}
+}
