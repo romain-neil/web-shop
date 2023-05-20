86dd9866c3638070770ef5adac8ce19557f41307
Romain Neil <contact@romain-neil.fr>
2023-05-20 21:23:50 +0200

    refactor: make services class stringable

--- src/Entity/AbstractService.php
+++ src/Entity/AbstractService.php
@@ -13,7 +13,7 @@ use Doctrine\ORM\Mapping as ORM;
 	'mumble' => 'App\Entity\Services\Mumble\MumbleService',
 	'wireguard' => 'App\Entity\Services\Wireguard\WireguardService',
 ])]
-abstract class AbstractService {
+abstract class AbstractService implements \Stringable {
 
 	#[ORM\Id]
 	#[ORM\GeneratedValue]
