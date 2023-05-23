648980df2f1d634e547f731f07ca32a0c68ce62f
Romain Neil <contact@romain-neil.fr>
2023-05-23 10:42:15 +0200

    refactor: remove unused imports

--- src/Auth/Entity/User.php
+++ src/Auth/Entity/User.php
@@ -3,8 +3,6 @@ namespace Auth\Entity;
 
 use Auth\Repository\UserRepository;
 use DateTimeInterface;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Scheb\TwoFactorBundle\Model\Totp\TotpConfiguration;
 use Scheb\TwoFactorBundle\Model\Totp\TotpConfigurationInterface;
