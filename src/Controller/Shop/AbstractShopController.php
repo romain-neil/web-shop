a6260a0505a84b2b3e687c9868e2450ec4023413
Romain Neil <contact@romain-neil.fr>
2023-12-25 22:59:09 +0100

    refactor: extract order placing

--- src/Controller/Shop/AbstractShopController.php
+++ src/Controller/Shop/AbstractShopController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller\Shop;
 
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 abstract class AbstractShopController extends ShopController {
@@ -10,7 +9,5 @@ abstract class AbstractShopController extends ShopController {
 	abstract public function landingPage(): Response;
 
 	abstract public function selectPlan(): Response;
-
-    abstract public function placeOrder(int $id, Request $request): Response;
 
 }
