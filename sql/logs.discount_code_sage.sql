d7ca793f03fef4cbbf375df70d4a32e223f47ceb
Romain Neil <contact@romain-neil.fr>
2024-02-10 01:01:27 +0100

    feat(logs): discount code usage table

---
+++ sql/logs.discount_code_sage.sql
@@ -0,0 +1,17 @@
+CREATE VIEW logs.discount_usage
+AS
+    SELECT
+        usage.id AS id,
+        usage.date_usage AS date_code_used,
+
+        -- related order
+        o.date_created AS date_order_created,
+
+        -- related customer
+        CONCAT(user.prenom, ' ', UPPER(user.nom)) AS customer_name
+    FROM
+        shop.discount_code_usage AS usage
+    LEFT JOIN shop.order AS o ON o.id = usage.order_id
+    LEFT JOIN intranet.customer AS cust ON cust.id = o.customer_id
+    LEFT JOIN intranet.user AS user ON user.id = cust.id
+;
