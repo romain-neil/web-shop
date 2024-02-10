1b70225dfb458f26874e200bae8e42a52f0839f6
Romain Neil <contact@romain-neil.fr>
2024-02-10 19:01:10 +0100

    feat: base discount service

---
+++ src/Service/DiscountService.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Discount;
+use App\Entity\Order;
+use App\Repository\DiscountCodeUsageRepository;
+
+class DiscountService {
+
+	private DiscountCodeUsageRepository $repo;
+
+	public function __construct(DiscountCodeUsageRepository $repo) {
+		$this->repo = $repo;
+	}
+
+	/**
+	 * Does the given discount can be applied to the specified order ?
+	 * @param Discount $discount
+	 * @param Order $order
+	 * @return bool
+	 */
+	public function canApply(Discount $discount, Order $order): bool {
+		//Check if code is allowed to be used multiple times in current order
+		if (!$discount->isAllowMultipleUse()) {
+			$codeUsages = $order->getDiscountCodeUsages();
+
+			foreach ($codeUsages as $codeUsage) {
+				if ($codeUsage->getCode() === $discount) {
+					return false;
+				}
+			}
+		}
+
+		$min = $discount->getMinimumOrderAmount();
+		if ($min != null && $min > $order->getTotal()) {
+			return false;
+		}
+
+		//Get code usage
+		$usage = $this->repo->findBy(['code' => $discount]);
+		if ($usage === null) {
+			return true;
+		}
+
+		//TODO: may check how many code an user can enter
+
+
+		// Maximum code usage
+		if ($discount->getMaximalTotalUsage() >= count($usage)) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Function to add the discount only to applicable service, in current order
+	 * @TODO write it
+	 * @return void
+	 */
+	private function applyToService(): void {}
+
+}
