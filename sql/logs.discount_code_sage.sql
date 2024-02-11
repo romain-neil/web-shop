0b55a615a7dde16ae3028416d2dd8ac3adfb72e9
Romain Neil <contact@romain-neil.fr>
2024-02-11 22:34:46 +0100

    refactor: use deliminator

--- sql/logs.discount_code_sage.sql
+++ sql/logs.discount_code_sage.sql
@@ -8,10 +8,10 @@ AS
         o.date_created AS date_order_created,
 
         -- related customer
-        CONCAT(user.prenom, ' ', UPPER(user.nom)) AS customer_name
+        CONCAT("user".prenom, ' ', UPPER("user".nom)) AS customer_name
     FROM
         shop.discount_code_usage AS usage
     LEFT JOIN shop.order AS o ON o.id = usage.order_id
     LEFT JOIN intranet.customer AS cust ON cust.id = o.customer_id
-    LEFT JOIN intranet.user AS user ON user.id = cust.id
+    LEFT JOIN intranet."user" AS "user" ON "user".id = cust.id
 ;
