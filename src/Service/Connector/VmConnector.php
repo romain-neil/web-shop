d769516f39f5451c497619dd5fd4695ea6624697
Romain Neil <contact@romain-neil.fr>
2024-04-21 00:41:56 +0200

    wip: add other methods

--- src/Service/Connector/VmConnector.php
+++ src/Service/Connector/VmConnector.php
@@ -1,11 +1,18 @@
 <?php
 namespace App\Service\Connector;
 
+use App\Entity\Services\VirtualMachine\VmBackup;
 use App\Entity\Services\VirtualMachine\VmService;
 
 class VmConnector {
 
 	public function install(VmService $vm) {
+	}
+
+	public function restoreBackup(VmBackup $backup) {
+	}
+
+	public function removeBackup(VmBackup $backup) {
 	}
 
 }
