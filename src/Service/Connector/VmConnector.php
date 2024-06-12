310b100c230e4f86478d93ed7348d6667093e796
Romain Neil <contact@romain-neil.fr>
2024-06-12 20:14:42 +0200

    feat: base vm related functions

--- src/Service/Connector/VmConnector.php
+++ src/Service/Connector/VmConnector.php
@@ -3,16 +3,86 @@ namespace App\Service\Connector;
 
 use App\Entity\Services\VirtualMachine\VmBackup;
 use App\Entity\Services\VirtualMachine\VmService;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class VmConnector {
 
-	public function install(VmService $vm) {
+	public function __construct(private readonly ProxmoxConnector $proxmox) {}
+
+	public function install(VmService $vm): void {
+		//
 	}
 
-	public function restoreBackup(VmBackup $backup) {
+	/**
+	 * @throws TransportExceptionInterface
+	 * @throws \Exception
+	 */
+	public function createBackup(VmService $vm): void {
+		$server = $vm->getServer();
+
+		$req = $this->proxmox->getClient($server)->request('POST', '/api2/json/nodes/' . $server->getNodeId() . '/vzdump', [
+			'body' => [
+				'all' => 0,
+				'mode' => 'suspend',
+				'vmid' => $vm->getVmId()
+			]
+		]);
+
+		if ($req->getStatusCode() !== 200) {
+			throw new \Exception();
+		}
 	}
 
-	public function removeBackup(VmBackup $backup) {
+	public function restoreBackup(VmBackup $backup): void {
+		$server = $backup->getVm()->getServer();
+	}
+
+	public function removeBackup(VmBackup $backup): void {
+		$server = $backup->getVm()->getServer();
+	}
+
+	/**
+	 * @param VmService $vm
+	 * @return void
+	 * @throws TransportExceptionInterface
+	 * @throws \Exception
+	 */
+	public function start(VmService $vm): void {
+		$server = $vm->getServer();
+
+		$req = $this->proxmox->getClient($server)->request('POST', '/api2/json/nodes/' . $server->getNodeId() . '/qemu/' . $vm->getVmId() . '/status/start');
+
+		if ($req->getStatusCode() !== 200) {
+			throw new \Exception();
+		}
+	}
+
+	/**
+	 * @throws TransportExceptionInterface
+	 * @throws \Exception
+	 */
+	public function shutdown(VmService $vm): void {
+		$server = $vm->getServer();
+
+		$req = $this->proxmox->getClient($server)->request('POST', '/api2/json/nodes/' . $server->getNodeId() . '/qemu/' . $vm->getVmId() . '/status/shutdown');
+
+		if ($req->getStatusCode() !== 200) {
+			throw new \Exception();
+		}
+	}
+
+	/**
+	 * @throws TransportExceptionInterface
+	 * @throws \Exception
+	 */
+	public function stop(VmService $vm): void {
+		$server = $vm->getServer();
+
+		$req = $this->proxmox->getClient($server)->request('POST', '/api2/json/nodes/' . $server->getNodeId() . '/qemu/' . $vm->getVmId() . '/status/stop');
+
+		if ($req->getStatusCode() !== 200) {
+			throw new \Exception();
+		}
 	}
 
 }
