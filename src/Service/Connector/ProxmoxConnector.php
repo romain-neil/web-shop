12e58652782dcf99d5164987ec58319b3584a658
Romain Neil <contact@romain-neil.fr>
2024-06-27 15:29:21 +0200

    refactor: use correct auth header

--- src/Service/Connector/ProxmoxConnector.php
+++ src/Service/Connector/ProxmoxConnector.php
@@ -4,14 +4,14 @@ namespace App\Service\Connector;
 use App\Entity\Server;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
-class ProxmoxConnector {
+readonly class ProxmoxConnector {
 
 	public function __construct(private HttpClientInterface $client) {}
 
 	public function getClient(Server $server): HttpClientInterface {
 		return $this->client->withOptions([
 			'headers' => [
-				'X-Api-Token' => $server->getApiKey()
+				'Authorization' => $server->getApiKey()
 			],
 			'base_uri' => $server->getApiUrl()
 		]);
