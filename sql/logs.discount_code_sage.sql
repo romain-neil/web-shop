db1112b199fb02bb440147935ee07f16f0a1980c
Romain Neil <contact@romain-neil.fr>
2026-06-05 15:24:09 +0200

    feat: create logs schema and update discount_usage view

--- sql/logs.discount_code_sage.sql
+++ sql/logs.discount_code_sage.sql
@@ -1,16 +1,17 @@
-CREATE VIEW logs.discount_usage
-AS
-    SELECT
-        usage.id AS id,
-        usage.date_usage AS date_code_used,
+CREATE SCHEMA IF NOT EXISTS logs;
 
-        -- related order
-        o.date_created AS date_order_created,
+CREATE OR REPLACE VIEW logs.discount_usage AS
+SELECT
+    usage.id AS id,
+    usage.date_usage AS date_code_used,
 
-        -- related customer
-        CONCAT("user".prenom, ' ', UPPER("user".nom)) AS customer_name
-    FROM
-        shop.discount_code_usage AS usage
+    -- related order
+    o.date_created AS date_order_created,
+
+    -- related customer
+    CONCAT("user".prenom, ' ', UPPER("user".nom)) AS customer_name
+FROM
+    shop.discount_code_usage AS usage
     LEFT JOIN shop.order AS o ON o.id = usage.related_order_id
     LEFT JOIN intranet.customer AS cust ON cust.id = o.customer_id
     LEFT JOIN intranet."user" AS "user" ON "user".id = cust.id
